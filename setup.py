96ab5052c6c51d1096c947eb7422e409d8ff3f75
mariuccio <mariotaddei@rocketmail.com>
2015-11-04 18:50:13 +0100

    package name

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-business-time',
     version='0.1',
-    packages=['business-time'],
+    packages=['business_time'],
     include_package_data=True,
     license='BSD License',  # example license
     description='An application to calculate business timedelta',
